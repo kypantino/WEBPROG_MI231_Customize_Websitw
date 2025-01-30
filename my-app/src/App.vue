7ecfb26290a3a92ea957089ea14bb040a34073e7
kypantino <kylaamorpantino@gmail.com>
2025-01-30 09:09:20 +0000

    add comments

---
+++ my-app/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const countries = ref([])
+
+async function getComments() {
+  const { data } = await supabase.from('comments').select()
+  commentss.value = data
+}
+
+onMounted(() => {
+  getComments()
+})
+</script>
+
+<template>
+  <ul>
+    <li v-for="comments in comments" :key="comments.id">{{ comment.name }} </li>
+  </ul>
+</template>
